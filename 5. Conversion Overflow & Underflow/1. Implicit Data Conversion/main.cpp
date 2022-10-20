bc58525574d6f0ed43bbf3bf038f13d4dc4feb18
Mehedi Hasan Munna <munnamehedihasan96@gmail.com>
2022-10-20 18:21:00 +0600

    5th

--- 5. Conversion Overflow & Underflow/1. Implicit Data Conversion/main.cpp
+++ 5. Conversion Overflow & Underflow/1. Implicit Data Conversion/main.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
+    
+	//		. The compiler applies implicit conversions
+	// 			when types are different in 
+	//			an expression
+	//		. Conversions are always done from the smallest
+	//			to the largest type in this case int is 
+	//			transformed to double before the expression
+	//			is evaluated.Unless we are doing an assignment
+	
+	double price { 45.6 };
+    int units {10};
+    
+    auto total_price = price * units; // units will be implicitly converted to double
+	
+	cout << "Total price : " << total_price << endl;
+	cout << "sizeof total_price : " << sizeof(total_price) << endl;
+
+
+	//Implicit conversions in assignments
+	// The assignment operation is going to cause an implicit
+	// narrowing conversion , y is converted to int before assignment
+	int x;
+    double y {45.44};
+    x = y; // double to int
+	cout << "The value of x is : " << x << endl; // 45
+	cout << "sizeof x : " << sizeof(x) << endl;// 4
 
 
     return 0;
--- 5. Conversion Overflow & Underflow/3.Overflow & Underflow/main.cpp
+++ 5. Conversion Overflow & Underflow/3.Overflow & Underflow/main.cpp
@@ -2,7 +2,38 @@
 using namespace std;
 int main()
 {
+  //Overflow 
 
+    unsigned char data {250};
+
+    ++data;
+    cout << "data : " << static_cast<int>(data) << endl;
+
+    ++data;
+    cout << "data : " << static_cast<int>(data) << endl;
+
+    ++data;
+    cout << "data : " << static_cast<int>(data) << endl;
+
+    ++data;
+    cout << "data : " << static_cast<int>(data) << endl;
+
+    ++data; 
+    cout << "data : " << std::hex <<  static_cast<int>(data) << endl; // 255
+
+    ++data;  // Overflow
+   cout << "data : " << static_cast<int>(data) << std::endl; // 256
+
+    cout << std::dec ;
+
+
+    data = 1;
+
+    --data;
+    cout << "data : " << static_cast<int>(data) << endl;
+
+    --data;
+    cout << "data : " << static_cast<int>(data) << endl;
 
     return 0;
 }
